b79129853b8a0f81781b338f48f46fdf815798be
florian bernier <florianbernier.soa@gmail.com>
2024-03-28 15:35:29 +0100

    validates

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :address, presence: true
+  validates :price_per_day, presence: true, numericality: { greater_than: 0 }
 end
